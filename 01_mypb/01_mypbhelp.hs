4560e8f9e98006da267d65076be8b4bb091b057e
YUZURANIUM <aspel.algon@gmail.com>
2022-11-11 15:22:55 +0900

    更新
    Signed-off-by: YUZURANIUM <aspel.algon@gmail.com>

---
+++ 01_mypb/01_mypbhelp.hs
@@ -0,0 +1,11 @@
+; INFO ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+; FileName : 01_mypbhelp.hs
+; Version  : 0.1.1
+; Date     : 2022/11/11
+; Author   : YUZURANIUM（ゆずらにうむ）
+; Twitter  : https://twitter.com/YUZRANIUM
+; GitHub   : https://github.com/YUZURANIUM/01_mypb
+;━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+; Description
+; HSP3でプログレスバーを設置するモジュールのヘルプファイルです。
+;━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
